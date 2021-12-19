1f53fb8161e10e32e066e5ec07cc1161f50c049d
ofekSaar <ofeksaar87@gmail.com>
2021-12-19 22:40:10 +0200

    add route to all

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 ReactDOM.render(
   <Provider store = {store}>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+      <Router>
+        <App />
+      </Router>
+    </React.StrictMode>
   </Provider>,
   document.getElementById('root')
 );
