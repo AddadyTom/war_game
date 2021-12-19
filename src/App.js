267cfba79effbe144d3183a0072a30b025f012ac
ofekSaar <ofeksaar87@gmail.com>
2021-12-19 22:16:29 +0200

    s

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
+import * as React from 'react';
+import SignInSide from './pages/SignInSide';
+import {
+  Routes,
+  Route,
+  Navigate
+} from "react-router-dom";
+import GameSelection from './pages/GameSelection';
 
-import './App.css';
-import {connect , dispatch} from 'react-redux'
-import Number from './number'
-import SettingStepper from './SettingsStepper/SettingStepper'
 
+function App() {
+  
+  const [loggedIn, setLoggedIn] = React.useState(false);
 
-const App = (props) => {
-  return (
-   <SettingStepper>
-   </SettingStepper>
-  )
+  
+  if(loggedIn) {
+    return (
+      <Routes>
+        <Route path='/selection' element={<GameSelection/>} />
+        <Route path='/joinGame' element={<GameSelection/>} />
+        <Route path='/createGame' element={<GameSelection/>} />
+        <Route path='/home' element={<GameSelection/>} />
+        <Route path="/login" element={<Navigate to ="/selection" />}/>
+        <Route path="*" element={<Navigate to ="/selection" />}/>
+      </Routes>
+    );
+  }
+  else {
+    return (
+      <Routes>
+        <Route path='/login' element={<SignInSide setLoggedIn={setLoggedIn}/>} />
+        <Route path="*" element={<Navigate to ="/login" />}/>
+      </Routes>  
+    );
+  }
 }
 
-
-
-export default  App
+export default App;
